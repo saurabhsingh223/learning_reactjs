78430ee082114c3bc886010a4dcb179eceee99ec
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-11-14 01:10:34 +0530

    Counter Project using useState hook

---
+++ 02_counterproject/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  let [Counter, setCounter] = useState(0);
+
+  // let Counter = 15
+
+  const addValue = () => {
+    // Counter = Counter + 1;
+    if(Counter < 20){
+ setCounter(Counter+1)
+//  setCounter(Counter => Counter+1)  if we want to add 4 values at a single time
+//  setCounter(Counter => Counter+1)
+//  setCounter(Counter => Counter+1)
+    console.log(Counter);
+    }else{
+      alert("value cannot be more that 20")
+    }
+   
+  };
+
+  const removeValue = () => {
+    // console.log("value removed", Math.random());
+    // Counter = Counter - 1
+    if(Counter > 0){
+    setCounter(Counter-1)
+    console.log(Counter);
+    }else{
+      alert("value cannot be less than 0")
+    }
+    
+  };
+
+  return (
+    <>
+      <h1>Hello Counter</h1>
+      <h2>Counter Value: {Counter}</h2>
+
+      <button onClick={addValue}>Add Value: {Counter
+}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value: {Counter}</button>
+      <footer>footer: {Counter}</footer>
+    </>
+  );
+}
+
+export default App;
